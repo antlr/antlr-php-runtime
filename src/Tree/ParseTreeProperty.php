e8964b77a0b33afafb5b624c075a62ae699245c0
Herman Peeren <herman@yepr.nl>
2020-09-10 16:50:35 +0200

    Code style adjusted, PSR-12

--- src/Tree/ParseTreeProperty.php
+++ src/Tree/ParseTreeProperty.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Antlr\Antlr4\Runtime\Tree;
 
+use \SplObjectStorage;
+
 /**
  * Associate a property with a parse tree node.
  *
@@ -16,69 +18,68 @@ namespace Antlr\Antlr4\Runtime\Tree;
  *
  *     use Antlr\Antlr4\Runtime\Tree\ParseTreeProperty;
  *
- *     // Declare a ParseTreeProperty-container for values in your listener
+ *     // Declare a ParseTreeProperty-container for values in your listener.
  *     private $values;
  *
- *     // Make a new container in the constructor of the listener
+ *     // Make a new container in the constructor of the listener.
  *     $this->values = new ParseTreeProperty();
  *
- *     // Use it from any method in your listener
- *     // Use it to store or retrieve any value, associated to a specific node
+ *     // Use it from any method in your listener.
+ *     // Use it to store or retrieve any value, associated to a specific node.
  *     $this->values->put($node, $someValue);
  *     $aValue = $this->values->get($node);
  *     $this->values->removeFrom($node);
  *
  *
- * This class is implemented in PHP as a wrapper around \SplObjectStorage
+ * This class is implemented in PHP as a wrapper around \SplObjectStorage.
  */
-Class ParseTreeProperty
+class ParseTreeProperty
 {
-    protected $storage;
+	protected $storage;
 
-    public function  __construct()
-    {
-        $this->storage = new \SplObjectStorage();
-    }
+	public function __construct()
+	{
+		$this->storage = new SplObjectStorage();
+	}
 
-    /**
-     * Get the value associated with $node from the storage
-     *
-     * @param  ParseTree $node  The {@see ParseTree} with which the value is associated.
-     * @return mixed     $value The stored value | null when $node is not in the storage.
-     */
-    public function get(ParseTree $node)
-    {
-        $value = null;
-        if ($this->storage->contains($node))
-        {
-            $value = $this->storage->offsetGet($node);
-        }
-        return $value;
-    }
+	/**
+	 * Get the value associated with $node from the storage.
+	 *
+	 * @param ParseTree $node The {@see ParseTree} with which the value is associated.
+	 * @return mixed     $value The stored value | null when $node is not in the storage.
+	 */
+	public function get(ParseTree $node)
+	{
+		$value = null;
+		if ($this->storage->contains($node)) {
+			$value = $this->storage->offsetGet($node);
+		}
+		return $value;
+	}
 
-    /**
-     * Put a value associated with $node in the storage
-     *
-     * @param  ParseTree $node  The {@see ParseTree} with which the value is associated.
-     * @param  mixed     $value Any value
-     * @return void
-     */
-    public function put(ParseTree $node, $value): void
-    {
-        $this->storage->attach($node, $value);
-    }
+	/**
+	 * Put a value associated with $node in the storage.
+	 *
+	 * @param ParseTree $node The {@see ParseTree} with which the value is associated.
+	 * @param mixed $value Any value
+	 * @return void
+	 */
+	public function put(ParseTree $node, $value): void
+	{
+		$this->storage->attach($node, $value);
+	}
 
-    /**
-     * Remove the value associated with $node from the storage
-     *
-     * @param  ParseTree $node  The {@see ParseTree} with which the value is associated.
-     * @return mixed     $value The removed value  | null when $node was not in the storage.
-     */
-    public function removeFrom(ParseTree $node)
-    {
-        $value = $this->get($node);
-        $this->storage->detach($node);
-        return $value;
-    }
+	/**
+	 * Remove the value associated with $node from the storage.
+	 *
+	 * @param ParseTree $node The {@see ParseTree} with which the value is associated.
+	 * @return mixed     $value The removed value  | null when $node was not in the storage.
+	 */
+	public function removeFrom(ParseTree $node)
+	{
+		$value = $this->get($node);
+		$this->storage->detach($node);
+		return $value;
+	}
 
 }
